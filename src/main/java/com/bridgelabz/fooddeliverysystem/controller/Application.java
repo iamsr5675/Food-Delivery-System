c5833f2eb22f49371d820213a9c309df2d6dbc23
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-15 11:17:39 +0530

    Refactor

--- src/main/java/com/bridgelabz/fooddeliverysystem/controller/Application.java
+++ src/main/java/com/bridgelabz/fooddeliverysystem/controller/Application.java
@@ -27,29 +27,29 @@ public class Application {
 				userInterface.print(foodStore.getFoodList());
 				try {
                     if (foodStore.getFoodList().get(0) != null) {
-				System.out.println("Enter the name which you want to remove:");
-				String foodName = new Scanner(System.in).nextLine();
-				foodStore.remove(foodName);
-				break;
+                    	System.out.println("Enter the name which you want to remove:");
+                    	String foodName = new Scanner(System.in).nextLine();
+                    	foodStore.remove(foodName);
+                    	break;
                     }
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("No item present of that name to remove");
-                }
+                    	} catch (IndexOutOfBoundsException e) {
+                    		System.out.println("No item present of that name to remove");
+                    		}
 			case 3:
 				userInterface.print(foodStore.getFoodList());
 				break;
 			case 4:
 				try {
 		            if (foodStore.getFoodList().get(0) != null) {
-				System.out.println("Enter the name which you want to edit: ");
-                String name = new Scanner(System.in).nextLine();
-                FoodItem food = foodStore.getFoodItem(name);
-                userInterface.editItem(food);
-				break;
-		            }
-				} catch (IndexOutOfBoundsException e) {
-		            System.out.println("No item present of that name to edit");
-		        }
+		            	System.out.println("Enter the name which you want to edit: ");
+		            	String name = new Scanner(System.in).nextLine();
+		            	FoodItem food = foodStore.getFoodItem(name);
+		            	userInterface.editItem(food);
+		            	break;
+		            	}
+		            } catch (IndexOutOfBoundsException e) {
+		            	System.out.println("No item present of that name to edit");
+		            	}
 			case 5:
 				break;
 			}
